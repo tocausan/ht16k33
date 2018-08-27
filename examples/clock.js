b194f9eb25d438cf803c61dd5846df5820bb1ea0
tocausan <tocausan@gmail.com>
2018-08-27 22:55:57 +0200

    clock examples

--- examples/clock.js
+++ examples/clock.js
@@ -1,14 +1,8 @@
 const Segments = require('../library/14-segments'),
-    moment = require('moment'),
     display = new Segments(0x70, 1);
 
-let i = 1;
+// start the clock
+display.clock();
 
-setInterval(() => {
-    const time = moment().format('HHmm');
-    if (i > 4) i = 1;
-    let str = time.split('');
-    str.splice(i, 0, '.');
-    display.writeString(str.join(''));
-    i++;
-}, 250);
+// stop the clock after 30s
+setTimeout(() => display.clear(), 30000);
